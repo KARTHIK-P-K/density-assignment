a438e65716d275c0efbad6cdf11f9f9fa8b26103
KARTHIK-P-K <2001karthikpk@gmail.com>
2023-08-23 20:13:09 +0530

    more changes in nineth section

--- components/NinethSection.jsx
+++ components/NinethSection.jsx
@@ -28,16 +28,38 @@ const NinethSection = () => {
     };
   }, []);
   return (
-    <div className="p-8">
+    <div className="p-2 md:p-4 lg:p-8">
       <div id="target-section6"></div>
-      <div className="bg-violet-50 rounded-3xl h-[800px] p-16">
-        <div className="flex justify-between ">
-          <h1 className="text-5xl font-bold">Work with us</h1>
-          <h1 className="text-blue-600 text-5xl font-bold">ahead</h1>
+      <div className="bg-violet-50 rounded-3xl h-[1200px] lg:h-[800px] p-4 md:p-8 lg:p-16">
+        <div className="flex flex-col lg:flex-row lg:justify-between ">
+          <AnimatePresence>
+            {isVisible && (
+              <motion.div
+                initial={{ opacity: 0, x: -150 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -150 }}
+                transition={{ duration: 1, ease: "easeOut" }}
+              >
+                <h1 className="text-5xl font-bold">Work with us</h1>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <AnimatePresence>
+            {isVisible && (
+              <motion.div
+                initial={{ opacity: 0, x: 150 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 150 }}
+                transition={{ duration: 1, ease: "easeOut" }}
+              >
+                <h1 className="text-blue-600 text-5xl font-bold">ahead</h1>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
-        <div className="flex justify-between px-20">
+        <div className="flex flex-col lg:flex-row justify-between px-4 lg:px-10 xl:px-20">
           <div className=" ">
-            <div className="flex flex-col gap-y-4 bg-white w-[600px] h-[250px] p-6 rounded-2xl mt-10">
+            <div className="flex flex-col gap-y-4 bg-white w-[300px] xl:w-[600px] h-[250px] p-6 rounded-2xl mt-10">
               <Image src={ahead10} width={50} height={50} alt="ahead10" />
               <h1 className="text-xl font-bold">About</h1>
               <p>
@@ -46,7 +68,7 @@ const NinethSection = () => {
                 all about.
               </p>
             </div>
-            <div className="flex  flex-col  gap-y-4 bg-amber-50 w-[600px] h-[150px] p-6  ">
+            <div className="flex  flex-col  gap-y-4 bg-amber-50 w-[300px] xl:w-[600px] h-[170px] p-6  ">
               <h1 className="text-xl font-bold">Product</h1>
               <p>
                 At ahead our goal is to make self improvement fun and lasting.
